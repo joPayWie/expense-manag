45eaea8790462cec2b750df66fb882c93b2cfd67
joPay <josefina.payer@gmail.com>
2022-10-17 18:46:47 -0300

    finishing burger menu, adding report and tag section and hidden them

--- js/script.js
+++ js/script.js
@@ -8,10 +8,17 @@ const $$ = (selector) => documento.querySelectorAll(selector)
 // Burger menu variables
  const burgerBtn = $("button.mobile-menu-button")
  const burgerMenu = $(".mobile-menu")
+ const burgerIconLines = $("#burger-icon-lines")
+ const burgerIconX = $("#burger-icon-xmark")
 
  // Burger menu event listeners
  burgerBtn.addEventListener("click", () => {
-    burgerMenu.classList.toggle("hidden");
+   burgerMenu.classList.toggle("hidden");
+   burgerIconLines.classList.toggle("hidden")
+   burgerIconX.classList.toggle("hidden")   
  })
+
+
+ /********************* Sections click events *************/
  
  
